5f93ef8b8bb05f59fb85f8420d9744869d180a85
CharlyBoyz <cbusiness@hotmail.fr>
2023-11-13 00:09:19 +0100

    git ignore

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -80,7 +80,7 @@
     </div>
     
     <!-- Responsive Navigation Menu -->
-    <div x-show="open" x-transition.duration.500ms class="sm:hidden">
+    <div x-show="open" x-transition.duration.500ms class="sm:hidden ">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
